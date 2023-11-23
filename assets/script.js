32987e821a771b1c695526990e74f2c681ac499f
Cooper Griffin <coopergriffin@cmail.carleton.ca>
2023-11-23 18:29:38 -0500

    Updated script.js
    Now returns random password off a set char list with a length given by the user

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,15 @@ var generateBtn = document.querySelector("#generate");
 
 // function to call generatePassword() function and to add it to the HTML
 function writePassword() {
-  var password = generatePassword();
+  //Gets requested password length from the user and converts answer into an int
+  var passwordLengthStr = prompt("Enter the password length (between 8 and 128):", "8");
+  var passwordLength = parseInt(passwordLengthStr);
+
+  if (passwordLength === null || isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Invalid password length. Please enter a number between 8 and 128.");
+    return;
+  }
+  var password = generatePassword(passwordLength);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -17,8 +25,16 @@ function writePassword() {
 }
 
 //function to generate and return a string password
-function generatePassword() {
-  return "password123"
+function generatePassword(pLength) {
+
+  //bass charset, will update later
+  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    var password = "";
+    for (var i = 0; i < pLength; i++) {
+        var randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset.charAt(randomIndex);
+    }
+    return password;
 }
 
 // Add event listener to call function writePassword when clicked 
