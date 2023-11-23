54ad6bab481a1ab12605da081dd027ca6b75ce87
Cooper Griffin <coopergriffin@cmail.carleton.ca>
2023-11-23 17:42:05 -0500

    Updated comments
    Added more comments to JS file for developer readability and understanding

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,13 @@
-// Assignment code here
+//Cooper Griffin 
+//November 23rd 2023 
+//Javascript file for challengeThree project 
+//Main purpose to generate and return string password to the HTML/frontend 
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// function to call generatePassword() function and to add it to the HTML
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -13,9 +16,10 @@ function writePassword() {
 
 }
 
+//function to generate and return a string password
 function generatePassword() {
   return "password123"
 }
 
-// Add event listener to generate button
+// Add event listener to call function writePassword when clicked 
 generateBtn.addEventListener("click", writePassword);
